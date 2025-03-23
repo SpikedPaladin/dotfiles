ec062fbff0f41dd97e8c01442c1db766f8137af4
PaladinDev <paladingames1@gmail.com>
2025-03-23 11:37:40 +0300

    Added NvimTree

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,5 +1,8 @@
 local map = vim.keymap.set
 
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+
 if vim.fn.executable("lazygit") == 1 then
     map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
 end
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,15 @@
 return {
     {
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	dependencies = {
+	    "nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+	    require("nvim-tree").setup()
+	end,
+    },
+    {
         "nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
