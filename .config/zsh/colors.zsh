6fa59996b6d5e6193cfd06c79b828c4b20efa635
Alexander Davydzik <paladindev@altlinux.org>
2025-03-25 14:55:11 +0300

    zsh: Reformated configuration

---
+++ .config/zsh/colors.zsh
@@ -0,0 +1,10 @@
+typeset -gA ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[alias]='fg=10'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=10'
+ZSH_HIGHLIGHT_STYLES[command]='fg=10'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=10,underline'
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=9'
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=14'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=14'
+
+ZSH_THEME="granite"
