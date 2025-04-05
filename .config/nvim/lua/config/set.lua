d42393e0538165d32bc6c5238e6c643075c13e71
PaladinDev <paladingames1@gmail.com>
2025-04-05 21:19:16 +0300

    nvim: Updated config
    Added more options; Updated snacks config

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -12,5 +12,10 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = false
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 4
+
 vim.opt.wrap = false
 vim.g.editorconfig = false
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,42 @@ return {
         priority = 1000,
         lazy = false,
         opts = {
-            dashboard = { enabled = true },
+            dashboard = {
+                preset = {
+                    pick = nil,
+                    ---@type snacks.dashboard.Item[]
+                    keys = {
+                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
+                    header = [[
+                                                                             
+               ████ ██████           █████      ██                     
+              ███████████             █████                             
+              █████████ ███████████████████ ███   ███████████   
+             █████████  ███    █████████████ █████ ██████████████   
+            █████████ ██████████ █████████ █████ █████ ████ █████   
+          ███████████ ███    ███ █████████ █████ █████ ████ █████  
+         ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+                    ]],
+                },
+                sections = {
+                    { section = 'header' },
+                    {
+                        section = "keys",
+                        indent = 1,
+                        padding = 1,
+                    },
+                    { section = 'recent_files', icon = ' ', title = 'Recent Files', indent = 3, padding = 2 },
+                    { section = "startup" },
+                },
+            },
             indent = { enabled = true },
             notifier = { enabled = true },
             explorer = {
@@ -18,6 +53,8 @@ return {
             { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
             { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
             { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+            -- Buffer
+            { "<leader>bd", function() Snacks.bufdelete() end, desc = "Buffer Delete" },
             -- LSP
             { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
             { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
