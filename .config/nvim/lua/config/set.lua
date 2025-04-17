a05c63839e1759e5d84f90aa08bb88d6bd49a65a
Alexander Davydzik <paladindev@altlinux.org>
2025-04-17 15:20:03 +0300

    nvim: QoL update

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -5,6 +5,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Turn off the stupidest thing in the entire universe
+vim.g.meson_recommended_style = 0
+
 -- Tabs to 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -21,3 +24,4 @@ vim.opt.wrap = false
 vim.g.editorconfig = false
 
 vim.g.neovide_normal_opacity = 0
+vim.g.neovide_hide_mouse_when_typing = true
--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,4 +1,5 @@
 return {
     { "thetek42/vim-blueprint-syntax" },
-    { "vala-lang/vala.vim" }
+    { "vala-lang/vala.vim" },
+    { "nvim-tree/nvim-web-devicons", opts = {} }
 }
